f31788a7c56fc9fcd675bb36fb7aa979b6b19475
bonobo <arburpee@gmail.com>
2022-02-24 15:11:03 -0800

    added lib directory for routes functions

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,26 +1,37 @@
 const router = require('express').Router();
-const { Notes } = require('../../data/db.json');
+const { notes } = require('../../data/db.json');
+const { findById, createNewNote } = require('../../lib/notes');
 
+//GET AND POST /api/notes
 
-router.get('/', (req, res) => {
-    Notes.findAll({})
-    .then(dbNotesData => res.json(dbNotesData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+router.get('/notes', (req, res) => {
+    // Notes.findAll({})
+    // .then(dbNotesData => res.json(dbNotesData))
+    // .catch(err => {
+    //     console.log(err);
+    //     res.status(500).json(err);
+    // });
+    let results = notes;
+    res.json(results);
 });
 
-router.post('/', (req, res) => {
-    Notes.create({
-        title: req.body.title,
-        text: req.body.text
-    })
-    .then(dbNotesData => res.json(dbNotesData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+})
+
+router.post('/notes', (req, res) => {
+    if (!notes) {
+        req.body.id = 1
+    } else {
+    req.body.id = Notes.length.toString();
+    }
+    const note = createNewNote(req.body, notes);
+    res.json(note)
 });
 
 module.exports = router;
